97ab8157abb2e00cad3284c36ae8e45cc6e20889
Baskin-Ridley <gbaskinridley@gmail.com>
2022-10-16 16:08:57 +0100

    added photo

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -6,7 +6,7 @@ const Main = () => {
     <div className="main">
         <h2>Hi, I'm Gabriel Baskin-Ridley</h2>
         <p>I am currently self-learning to be a full-stack webdeveloper.</p>
-
+        <img className="photo" src={"./gabriel.webp"} alt="{rofile of Gabriel Baskin-Ridley in the woods wearing glasses and a bomber jacket" />
         <ul>
             <li>I have a History degree</li>
             <li>My favourite colour is green</li>
