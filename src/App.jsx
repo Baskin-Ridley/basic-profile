05eff1a365758d0e41ad8c9ad48ae81420995f1e
Baskin-Ridley <gbaskinridley@gmail.com>
2022-10-16 15:20:27 +0100

    created header template

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import Contact from './components/contact/Contact'
-
+import Header from './components/contact/header/Header'
 function App() {
 
   return (
     <div className="App">
-
+      <Header />
       <Contact />
 
 
