44b90d960d8797e8b09fd810953ee4038f10ec3d
Baskin-Ridley <gbaskinridley@gmail.com>
2022-10-16 15:38:04 +0100

    started building main

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Contact from './components/contact/Contact'
-import Header from './components/contact/header/Header'
+import Header from './components/header/Header'
+import Main from './components/main/Main'
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <Main />
       <Contact />
-
-
-
     </div>
   )
 }
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Main.css" 
+
+const Main = () => {
+  return (
+    <div className="main">
+        <h2>Hi, I'm Gabriel Baskin-Ridley</h2>
+        <p>I am currently self-learning to be a full-stack webdeveloper .</p>
+    </div>
+    )
+}
+
+export default Main
