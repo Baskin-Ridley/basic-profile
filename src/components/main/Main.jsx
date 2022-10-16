950302d98814828c9fd98c92aeda9f5b11f687cc
Baskin-Ridley <gbaskinridley@gmail.com>
2022-10-16 15:47:30 +0100

    basic styling of main

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -5,7 +5,14 @@ const Main = () => {
   return (
     <div className="main">
         <h2>Hi, I'm Gabriel Baskin-Ridley</h2>
-        <p>I am currently self-learning to be a full-stack webdeveloper .</p>
+        <p>I am currently self-learning to be a full-stack webdeveloper.</p>
+        <ul>
+            <li>I have a History degree</li>
+            <li>My favourite colour is green</li>
+            <li>3rd answer</li>
+        </ul>
+
+
     </div>
     )
 }
